08ef150325ca86043cf34dd07441a181e385fae2
panhzh3 <haizhipan@gmail.com>
2016-08-17 09:01:48 +0800

    fix service error

--- app/service/news_service.py
+++ app/service/news_service.py
@@ -5,9 +5,9 @@ from ..model.news_category import NewsCategory
 from ..model.news import News
 
 def get_news_list(category):
-    query = category.news.query if category else News.query
-    return query.order_by(db.desc('id'))\
-            .all()
+    news = category.news if category else News.query.all()
+    news = sorted(news, lambda a,b:a.id>b.id)
+    return news
 
 def get_news(news_id):
     return News.query.filter_by(id=news_id).first()
