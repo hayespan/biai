139a4e289d3f7accc4df956728f8902e921d3d5b
panhzh3 <haizhipan@gmail.com>
2016-08-26 01:40:13 +0800

    update

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
 	var bannerSwiper = new Swiper('.banner-block .swiper-container', {
-		prevButton:'.swiper-button-prev',
-		nextButton:'.swiper-button-next',
+		prevButton:'.banner-block .swiper-button-prev',
+		nextButton:'.banner-block .swiper-button-next',
 		autoplay: 5000,
 		loop: true,
 	});
 
 	var videoSwiper = new Swiper('.video-menu .swiper-container', {
 		direction: 'vertical',
+		prevButton:'.video-menu .swiper-button-prev',
+		nextButton:'.video-menu .swiper-button-next',
         slidesPerView: 3,
-        spaceBetween: 12,
-		autoplay: 5000
+        spaceBetween: 12
 	});
 })
 
--- mrun.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from app import App
-app = App()
-realapp = app.app
-
-if __name__ == '__main__':
-    realapp.run(host='0.0.0.0')
