132d202d522876b02a96f4f9900ea39cfcb7d1fd
panhzh3 <haizhipan@gmail.com>
2016-08-26 01:19:29 +0800

    mobile run

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
 	var bannerSwiper = new Swiper('.banner-block .swiper-container', {
-		prevButton:'.banner-block .swiper-button-prev',
-		nextButton:'.banner-block .swiper-button-next',
+		prevButton:'.swiper-button-prev',
+		nextButton:'.swiper-button-next',
 		autoplay: 5000,
 		loop: true,
 	});
 
 	var videoSwiper = new Swiper('.video-menu .swiper-container', {
 		direction: 'vertical',
-		prevButton:'.video-menu .swiper-button-prev',
-		nextButton:'.video-menu .swiper-button-next',
         slidesPerView: 3,
-        spaceBetween: 12
+        spaceBetween: 12,
+		autoplay: 5000
 	});
 })
 
