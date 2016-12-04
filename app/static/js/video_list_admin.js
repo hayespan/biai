45b99afc35041d1ec27cb462f9379ea892f30021
lucifan <fanshaowolf@163.com>
2016-12-05 01:32:38 +0800

    add process bar for video upload

--- app/static/js/video_list_admin.js
+++ app/static/js/video_list_admin.js
@@ -49,17 +49,32 @@ $(document).ready(function() {
 		var weight = $(this).parents('.video-item').find('.weight').val();
 		var pic = $(this).parents('.video-item').find('.pic')[0].files[0];
 		var video = $(this).parents('.video-item').find('.video')[0].files[0];
+		var bar = $(this).parents('.video-item').find('.process-bar');
 		var form_data = new FormData();
 		form_data.append('name', name);
 		form_data.append('weight', weight);
 		form_data.append('pic', pic);
 		form_data.append('video', video);
+		$(this).parents('.video-item').find('.item-controller').hide();
+		$(this).parents('.video-item').find('.process-bar-container').show();
 		$.ajax({
 			url : '/admin/video/create?f=json',
 			type : 'POST',
 			data : form_data,
 		    processData: false,
 		    contentType: false,
+			xhr: function() {
+				var xhr = $.ajaxSettings.xhr();
+				xhr.upload.onprogress = function(evt) {
+					var progress = evt.loaded/evt.total*100;
+					bar.text(Math.floor(progress)+'%');
+					bar.css('width', 135*progress/100+'px');
+					// if (progress == 100) {
+					// 	location.reload();
+					// }
+				};
+				return xhr;
+			},
 			success : function(result) {
 				location.reload();
 			}
